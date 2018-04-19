e6a7cb9c1f0ffef16648edbed71fc71390a0f73e
Guillaume Filliere <gfilliere@gmail.com>
2018-04-19 11:22:27 +0200

    Handle comments on columns

--- src/instrospection/columns.js
+++ src/instrospection/columns.js
@@ -9,6 +9,7 @@ export default (pool: any, oid: string) =>
     `
 SELECT a.attname,
   pg_catalog.format_type(a.atttypid, a.atttypmod) as type,
+  pg_catalog.col_description('${oid}', a.attnum) as comment,
   (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)
    FROM pg_catalog.pg_attrdef d
    WHERE d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef) as default,
@@ -16,5 +17,12 @@ SELECT a.attname,
 FROM pg_catalog.pg_attribute a
 WHERE a.attrelid = '${oid}' AND a.attnum > 0 AND NOT a.attisdropped
 ORDER BY a.attnum;`,
-    raw => new Column(raw.attname, raw.type, raw.default, raw.attnotnull),
+    raw =>
+      new Column(
+        raw.attname,
+        raw.type,
+        raw.default,
+        raw.attnotnull,
+        raw.comment,
+      ),
   );
